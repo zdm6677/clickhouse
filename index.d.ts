657b67dce5d5edb752f95b09fa8d84033725262a
zdm <13095533692@163.com>
2019-09-11 10:06:48 +0800

    ts grunt error

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
 
 declare module 'clickhouse' {
-  import {Stream} from 'stream';
+  import { Stream } from 'stream';
 
   type callbackExec = (error: Error, rows?: Object[]) => void;
 
@@ -13,13 +13,13 @@ declare module 'clickhouse' {
   }
 
   export class WriteStream extends Stream.Transform {
-    writeRow(data: Array<any>);
-    exec();
+    writeRow(data: Array<any>): Promise<any>;
+    exec(): Promise<any>;
   }
 
   class QueryCursor {
     toPromise(): Promise<Object[]>;
-    exec(callback: callbackExec);
+    exec(callback: callbackExec):void;
     stream(): Stream | WriteStream;
   }
 }
